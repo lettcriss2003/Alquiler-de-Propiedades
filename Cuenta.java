0078978957f68744fd213fc3e74e1fffae8b659b
lettcriss2003 <104597520+lettcriss2003@users.noreply.github.com>
2022-12-28 15:29:33 -0500

    Add files via upload

---
+++ Cuenta.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author lettc
+ */
+public class Cuenta {
+    
+    private Integer id;
+    private String usuario;
+    private Boolean estado;
+
+    public Cuenta() {
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta{" + "id=" + id + ", usuario=" + usuario + ", estado=" + estado + '}';
+    }
+    
+        
+    
+    
+}
