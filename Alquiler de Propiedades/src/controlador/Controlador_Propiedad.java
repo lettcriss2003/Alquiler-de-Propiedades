f2d950f6c3863f8a59a83e3b64e3b4fb70dc9dff
LeoMaza <dalton.l.maza@unl.edu.ec>
2023-02-19 22:46:41 -0500

    Merge

---
+++ Alquiler de Propiedades/src/controlador/Controlador_Propiedad.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import modelo.Propiedad;
+
+/**
+ *
+ * @author lettc
+ */
+public class Controlador_Propiedad {
+    
+//    private Propiedad propiedad = new Propiedad();
+    
+    public Controlador_Propiedad(){
+    
+    }
+
+//    public Propiedad getPropiedad() {
+//        return propiedad;
+//    }
+//
+//    public void setPropiedad(Propiedad propiedad) {
+//        this.propiedad = propiedad;
+//    }
+
+    @Override
+    public String toString() {
+        return "Controlador_Propiedad{" + "propiedad=" + '}';
+    }
+    
+    
+    
+    
+}
